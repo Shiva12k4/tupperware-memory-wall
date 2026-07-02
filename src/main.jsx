a2cdb0ed7a8d377961508d3216ab889a1c61f942
Shiva12k4 <shivamshsrma20@gmail.com>
2026-07-02 13:01:22 +0530

    redirection

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from "react";
+import { StrictMode, location } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+
+import { useEffect } from "react";
+
+if (window.location.href.includes("ensrise.xyz")) {
+  window.location.replace("https://memory-wall.tupperwarebrands.com.my");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
